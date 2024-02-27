e5f21c54b353e1a0375df85177e0ad3b8befa726
Gabriel Rosa <gabr1sr@proton.me>
2024-02-27 15:17:56 -0300

    add: Nix and direnv support
    Closes #1

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Discord Bot";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShell = pkgs.mkShell {
+        nativeBuildInputs = [ pkgs.bashInteractive ];
+        buildInputs = with pkgs; [
+          nodePackages.prisma
+          nodePackages.pnpm
+          nodePackages.npm
+          nodejs-slim
+          bun
+          openssl
+        ];
+        shellHook = with pkgs; ''
+          export PRISMA_SCHEMA_ENGINE_BINARY="${prisma-engines}/bin/schema-engine"
+          export PRISMA_QUERY_ENGINE_BINARY="${prisma-engines}/bin/query-engine"
+          export PRISMA_QUERY_ENGINE_LIBRARY="${prisma-engines}/lib/libquery_engine.node"
+          export PRISMA_INTROSPECTION_ENGINE_BINARY="${prisma-engines}/bin/introspection-engine"
+          export PRISMA_FMT_BINARY="${prisma-engines}/bin/prisma-fmt"
+        '';
+      };
+    });
+}
