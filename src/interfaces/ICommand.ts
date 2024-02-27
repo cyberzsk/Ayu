9c07d2f9107e1377bca29e39a4d4f30762bbad2f
Gabriel Rosa <gabr1sr@proton.me>
2024-02-27 18:46:23 -0300

    fix: ICommandAction interface missing

--- src/interfaces/ICommand.ts
+++ src/interfaces/ICommand.ts
@@ -1,8 +1,9 @@
 import { type PermissionResolvable } from 'discord.js';
+import { type ICommandAction } from './ICommandAction';
 
 export interface ICommand {
     name: string;
-    action: CommandAction;
+    action: ICommandAction;
     isOwnerGuild: boolean;
     permission: PermissionResolvable[]
     aliases: string[];
---
+++ src/interfaces/ICommandAction.ts
@@ -0,0 +1,5 @@
+import { type Message } from 'discord.js';
+
+export interface ICommandAction {
+  (message: Message, args: string[]): void;
+}
