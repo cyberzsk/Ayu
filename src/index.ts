9e47f9f422e50e057153f46e1cb0684501eb5a44
Piod <lucciznirvax@proton.me>
2024-02-27 19:34:52 -0300

    ok

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,6 @@ import "dotenv/config"
 
 const token: string = process.env.DISCORD_TOKEN 
 
-const client: Ayu = new Ayu("MTIxMTg3NjM1NjA0OTMzODQyOA.GVxNz7.vyVyh8nI2QdQNEC52a1yNNPGCz4zdVPh5WT6Vs")
+const client: Ayu = new Ayu(token)
 
 client.run()
